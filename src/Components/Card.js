2636c91cd7139d911d90024dc18fae8cf7ecc5db
nagyb3 <nagybence2003@gmail.com>
2023-04-24 14:41:43 +0200

    add shopping cart button and remove amount from cards

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -22,11 +22,11 @@ export default function Card(props) {
             <img className="card-img" src={props.img} alt=""/>
             <p className="name" style={{textTransform: "capitalize"}}>{props.name}</p>
             <p className="price">{props.price} $</p>
-            <div className="quantity-input">
-                    <button data-testid="button-decrement" onClick={decrement} className="decrement">-</button>
-                    <input min="0" max="20" value={itemOrderQuantity} type="number" data-testid="state" name="number" id="number"/>
-                    <button data-testid="button-increment" onClick={increment} className="increment">+</button>
-            </div>
+            {/*<div className="quantity-input">*/}
+            {/*        <button data-testid="button-decrement" onClick={decrement} className="decrement">-</button>*/}
+            {/*        <input min="0" max="20" value={itemOrderQuantity} type="number" data-testid="state" name="number" id="number"/>*/}
+            {/*        <button data-testid="button-increment" onClick={increment} className="increment">+</button>*/}
+            {/*</div>*/}
             <button onClick={handleClick}>ADD TO CART</button>
         </div>
     )
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,20 @@
 import React from "react"
+import cart from "../images/cart.png"
 
 export default function Navbar() {
+
+    function handleClick() {
+
+    }
+
     return (
         <nav>
-            <h1>Shopping Cart</h1>
+            <h1><a href="/">Fictional Webstore</a></h1>
             <ul>
                 <li><a href="/">Home</a></li>
                 <li><a href="/shop">Shop</a></li>
             </ul>
+            <img onClick={handleClick} src={cart} alt="" height="25px" id="shopping-cart-image"/>
         </nav>
     )
 }
